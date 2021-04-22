25bd3deb8337cb3acc678f10b1f873752bf112d8
eddyRAJA <eddy-rajaonarivelo_student2021@wilder.school>
2021-04-22 23:46:47 +0200

    add cart management

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,8 +25,53 @@ class HomeController extends AbstractController
     {
         $productManager = new ProductManager();
         $products = $productManager->selectAll();
-        return $this->twig->render('Home/index.html.twig', [
+        return $this->twig->render('Cart/index.html.twig', [
             'products' => $products
         ]);
+    }
+
+    public function cart()
+    {
+        return $this->twig->render('Cart/cart.html.twig', [
+            'cart' => $this->cartInfos(),
+            'totalCart' => $this->cartInfos()
+            ]);
+    }
+    
+    public function addToCart(int $idProduct)
+    {
+        if (!empty($_SESSION['cart'][$idProduct])) {
+            $_SESSION['cart'][$idProduct]++;
+        } else { 
+            $_SESSION['cart'][$idProduct] = 1;
+        }
+        header('Location: /');
+    }
+
+    public function cartInfos()
+    {
+        $productManager = new ProductManager();
+        if (isset($_SESSION['cart'])) {
+            $cart = $_SESSION['cart'];
+            $cartInfos = [];
+            foreach ($cart as $id =>$quantity) {
+                $product = $productManager->selectOneById($id);
+                $product['quantity'] = $quantity;
+                $cartInfos[] = $product;
+            }
+            return $cartInfos;
+        }
+        return false;
+    }
+
+    public function getTotalCart()
+    {
+        $total = 0;
+        if ($this->cartIfos != false) {
+            foreach($this->cartInfos() as $product) {
+                $total += $product['price'] * $product['quantity'];
+            }
+        }
+        return $total;
     }
 }
