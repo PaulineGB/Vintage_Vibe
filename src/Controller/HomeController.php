b8853026bf70037c71ba298b234f1f51a9d7ee58
amine-haddad <seniortouco@outlook.fr>
2021-04-22 20:18:30 +0200

    fonctionnalité newsletter

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -33,32 +33,41 @@ class HomeController extends AbstractController
 
     public function newsLetter()
     {
-
+        $errors = [];
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $newsletterManager = new NewsletterManager();
-            $newsletter = [
-                'email' => $_POST['email'],
-            ];
-            $newsletterManager->insert($newsletter);
-            header('Location: /home/newsLetter');
-        }
-        return $this->twig->render('Home/newsLetter.html.twig');
-    }
+            $newsletter = array_map('trim', $_POST);
 
-      /**
-     * Display item informations specified by $id
-     *
-     * @param int $id
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    public function contactNewsLetter(int $id)
+            if (empty($newsletter['email'])) {
+                $errors[] = 'Indiquez votre adresse e-mail s\'il vous plait';
+            }
+            $newsletterContact = filter_var($newsletter['email'], FILTER_VALIDATE_EMAIL);
+            if ($newsletterContact === false) {
+                $errors[] = 'Veuillez renseigner une adresse mail valide!';
+            }
+
+            if (empty($errors)) {
+                $newsletterManager = new NewsletterManager();
+                $newsletter = [
+                    'email' => $_POST['email'],
+                ];
+
+                $sentence = 'Merci de vous etre inscrit à notre Newsletter avec cette adresse e-mail: '
+                    . $newsletter['email'] . ', nous vous contactons bientôt.';
+                $newsletterManager->insert($newsletter);
+                return $this->twig->render('Home/newsLetter.html.twig', [
+                    'sentence' => $sentence
+                ]);
+            }
+        }
+        return $this->twig->render('Home/newsLetter.html.twig', [
+            'errors' => $errors
+        ]);
+    }
+    public function contactNewsLetter()
     {
         $newsLetterManager = new NewsLetterManager();
-        $newsLetters = $newsLetterManager->selectOneById($id);
+        $newsLetters = $newsLetterManager->selectAll();
 
-        return $this->twig->render('Home/contactNewsLetter.html.twig', ['newsLetters' => $newsLetters]);
+        return $this->twig->render('Newsletter/index.html.twig', ['newsLetters' => $newsLetters]);
     }
 }
