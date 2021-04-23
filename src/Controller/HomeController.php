15f63ce20650fc5c5aadcb6e1bc2269aa05e59a0
eddyRAJA <eddy-rajaonarivelo_student2021@wilder.school>
2021-04-23 10:36:37 +0200

    cart finish

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,8 +9,6 @@
 
 namespace App\Controller;
 
-use App\Model\ProductManager;
-
 class HomeController extends AbstractController
 {
     /**
@@ -23,65 +21,6 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        $productManager = new ProductManager();
-        $products = $productManager->selectAll();
-        return $this->twig->render('Cart/index.html.twig', [
-            'products' => $products
-        ]);
-    }
-
-    public function cart()
-    {
-        return $this->twig->render('Cart/cart.html.twig', [
-            'cart' => $this->cartInfos(),
-            'totalCart' => $this->cartInfos()
-            ]);
-    }
-
-    public function addToCart(int $idProduct)
-    {
-        if (!empty($_SESSION['cart'][$idProduct])) {
-            $_SESSION['cart'][$idProduct]++;
-        } else {
-            $_SESSION['cart'][$idProduct] = 1;
-        }
-        header('Location: /');
-    }
-
-    public function deleteFromCart(int $idProduct)
-    {
-        $cart=$_SESSION['cart'];
-        if (!empty($cart[$idProduct])) {
-            unset($cart[$idProduct]);
-        }
-        $_SESSION['cart'] = $cart;
-        header('Location: /home/cart');
-    }
-
-    public function cartInfos()
-    {
-        $productManager = new ProductManager();
-        if (isset($_SESSION['cart'])) {
-            $cart = $_SESSION['cart'];
-            $cartInfos = [];
-            foreach ($cart as $id => $quantity) {
-                $product = $productManager->selectOneById($id);
-                $product['quantity'] = $quantity;
-                $cartInfos[] = $product;
-            }
-            return $cartInfos;
-        }
-        return false;
-    }
-
-    public function getTotalCart()
-    {
-        $total = 0;
-        if ($this->cartInfos != false) {
-            foreach ($this->cartInfos() as $product) {
-                $total += $product['price'] * $product['quantity'];
-            }
-        }
-        return $total;
+        return $this->twig->render('Home/index.html.twig');
     }
 }
